c8165ee592d466cddbacba39ada4651b0b70fadb
itsFrank <frankobrien11@yahoo.ca>
2016-08-14 23:29:34 +0200

    Added faculties

--- server/src/services/faculties.js
+++ server/src/services/faculties.js
@@ -12,12 +12,11 @@ exports.FacultyMap = {
             '1397940820223215': exports.SCIENCE,
             '1582091102085150': exports.MANAGEMENT
         },
-        tokens: {
-            SCIENCE: 'EAAXBClPmZCXABAAE8MlaQeCViHwdth0s9jq3VMEOWTiOsxYQmAGlK0q1ZAv9xZAGFotnbMTw2a11WRHNqtCxl4cuTxq5uJonPCvrgBFHYHl7ZA2Q2eKzYciiifnZBEQA0nMybYrc0VhmTjOMEPmj003y79XFcSirF0xgd9MZCtNAZDZD',
-            MANAGEMENT: 'EAAXBClPmZCXABAE0UAZBwkrJL0MtCVvDRaADfId0WH7bwKcjkp5t2xrxCKPaDcu7OcwWAcElrFiPSjMJ9GbX8erzkQmTbpUD84bjMAPc6PfZCNmf2Y6W3xbL6mxgRvkoKFkWRUBHoYmpprzs4weBsRAcwsMbFw6qMBAZAkAZB3QZDZD'
-        }
+        tokens: {}
     }
 };
+exports.FacultyMap.facebook.tokens[exports.SCIENCE] = 'EAAXBClPmZCXABAAE8MlaQeCViHwdth0s9jq3VMEOWTiOsxYQmAGlK0q1ZAv9xZAGFotnbMTw2a11WRHNqtCxl4cuTxq5uJonPCvrgBFHYHl7ZA2Q2eKzYciiifnZBEQA0nMybYrc0VhmTjOMEPmj003y79XFcSirF0xgd9MZCtNAZDZD';
+exports.FacultyMap.facebook.tokens[exports.MANAGEMENT] = 'EAAXBClPmZCXABAE0UAZBwkrJL0MtCVvDRaADfId0WH7bwKcjkp5t2xrxCKPaDcu7OcwWAcElrFiPSjMJ9GbX8erzkQmTbpUD84bjMAPc6PfZCNmf2Y6W3xbL6mxgRvkoKFkWRUBHoYmpprzs4weBsRAcwsMbFw6qMBAZAkAZB3QZDZD';
 function requestFaculty(req) {
     if (req.subdomains.length != 1)
         return null;
